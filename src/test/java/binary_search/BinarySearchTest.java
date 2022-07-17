e6bf0f6203cfff4da746cef4e25be7f0e7af85cb
david-kariuki <dkaris.k@gmail.com>
2022-07-17 11:15:14 +0300

    Added simple test for the findFirstOccurrence binary search method

--- src/test/java/binary_search/BinarySearchTest.java
+++ src/test/java/binary_search/BinarySearchTest.java
@@ -20,26 +20,26 @@ public class BinarySearchTest {
 
 	/**
 	 * Method to test successful binary search with message from Supplier<String>
+	 * @apiNote - False positive if value not found in array (Returns -1)
 	 */
 	@Test
-	void testBinarySearchReturn() {
+	void testBinarySearchReturnNotEquals() {
 
 		int targetNumber = 22; // Targeted number to get index
 		boolean targetBoundary = false;
 
 		// Int array
-		int[] numArr = {2, 4, 8, 13, 14, 17, 19, 22, 23, 25, 30, 32, 34, 35, 36, 38, 41, 44, 45, 46, 49};
+		int[] numArr = {2, 4, 8, 13, 14, 17, 19, 22, 22, 23, 25, 30, 32, 34, 35, 36, 38, 41, 44, 45, 46, 49};
 
 		// Boolean array
 		boolean[] boolArray = new boolean[]{false, false, false, true, true};
 
-		// False positive if value not found in array (Returns -1)
-		assertNotEquals(-1, BinarySearch.binarySearch(numArr, targetNumber),
-				() -> "Binary search should return index from integer array");
+		String message = "Binary search should return index >= 0 from integer array";
 
-		// False positive if value not found in array (Returns -1)
-		assertNotEquals(-1, BinarySearch.findBooleanBoundary(boolArray, targetBoundary),
-				() -> "Binary search should return index from integer array");
+		assertNotEquals(-1, BinarySearch.binarySearch(numArr, targetNumber), () -> message);
+
+		assertNotEquals(-1, BinarySearch.findBooleanBoundary(boolArray, targetBoundary), () -> message);
+
+		assertNotEquals(-1, BinarySearch.findFirstOccurrence(numArr, targetNumber), () -> message);
 	}
-
 }
